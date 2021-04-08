45ba390abb5866f5262f09de2dd045dd1fcd990c
JMustang <eecfredes@gmail.com>
2021-04-08 20:50:08 -0300

    aula de String Types

---
+++ modulo-02/stringTypes/index.js
@@ -0,0 +1,14 @@
+"use strict";
+/**
+ * String Types
+ * Sao arquivos responsaveis por ensinar conseitos basicos sobre 'tipo string'
+ */
+// Exemplo 01 - Single Quotes
+let nomeCompleto = "junior carvalho";
+console.log(nomeCompleto);
+// Exemplo 02 - Double Quotes
+let funcaoEmpresa = "Cloud Advocate em JavaScript";
+console.log(funcaoEmpresa);
+// Exemplo 03 - Back Ticks
+let nomeEmpresa = "Twitter";
+console.log(`Ola ${nomeCompleto}, Seja bem-vindo ao ${nomeEmpresa}, sua funcao e ${funcaoEmpresa}`);
