e081ea3b91d562fc2c939dd3c1646f4390429640
JMustang <eecfredes@gmail.com>
2021-04-09 23:05:31 -0300

    Array Types

---
+++ modulo-02/arrayType/index.js
@@ -0,0 +1,33 @@
+"use strict";
+// Exemplo 01 - Uso de Colchetes:
+let frutas01 = ["abacaxi", "laranja", "maca", "melancia", "manga"];
+console.log(frutas01[2]);
+// Exemplo 02 - Array Object (Object Array):
+let frutas02 = [
+    "abacaxi",
+    "laranja",
+    "maca",
+    "melancia",
+    "manga",
+];
+console.log(frutas02[3]);
+// Exemplo 03 - Adicinando mais strings com metodo 'push:
+let idiomas = ["Portugues", "Ingles", "Frances", "Russo"];
+console.log(idiomas);
+idiomas.push("Mandarin");
+console.log(idiomas);
+// Exemplo 04 - Identificar tamanho do Array - metodo 'length':
+let idiomas00 = ["Portugues", "Ingles", "Frances", "Italiano"];
+console.log(idiomas00.length);
+// Exemplo 05 - Exeplo de Array com Spread Operator:
+let listaNumeros = [0, 1, 2, 3, 4, 5];
+listaNumeros = [...listaNumeros, 6, 7, 8, 9, 10];
+console.log(listaNumeros);
+// Exemplo 06 - Exeplo de Array com laco de iteracao:
+let linguagensArray = new Array("javascript", "java", "php", "rust");
+function funcaoLinguages(linguages) {
+    for (let i = 0; i < linguages.length; i++) {
+        console.log(linguagensArray[i]);
+    }
+}
+funcaoLinguages(linguagensArray);
